38c6af22a42e919adcee8e6c3257902bc3417884
aksalatdev <gothsaiko2@gmail.com>
2024-06-28 16:55:51 +0700

    fixin function tambahSaldo() more complex

--- bank_account.js
+++ bank_account.js
@@ -1,12 +1,13 @@
 let saldo = 0;
 
 function tambahSaldo() {
-  const jumlahTambah = parseFloat(window.prompt("Masukkan jumlah saldo yang ingin ditambahkan: "));
-  saldo += jumlahTambah;
-
-  console.log(`Saldo baru: Rp${saldo}`);
+    const jumlah = parseFloat(prompt("Masukkan jumlah saldo yang ingin ditambahkan:"));
+    if (!isNaN(jumlah)) {
+        bankAccount.deposit(jumlah);
+    } else {
+        alert("Masukan tidak valid. Silakan masukkan angka.");
+    }
 }
-
 
 function kurangiSaldo() {
   const jumlahKurang = parseFloat(window.prompt("Masukkan jumlah saldo yang ingin dikurangi: "));
